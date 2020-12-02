b63de9f9c82c3cdb80ca595356aedb6d8d42ee30
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-12-02 22:50:57 +0100

    validations

--- app/models/doll.rb
+++ app/models/doll.rb
@@ -1,6 +1,7 @@
 class Doll < ApplicationRecord
   belongs_to :user
   has_many :reviews
+  has_one :adoption, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
