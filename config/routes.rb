372889c780392e239d002c87d7129cd97b83c445
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-12-05 01:21:07 +0100

    routes for dolls and adoptions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dolls do
+    resources :adoptions
+  end
 end
